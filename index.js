bde45b43e2ac5369c5dd4d1e17e7e589dbaf893e
Ben Gorman <delingorman@gmail.com>
2021-09-23 11:27:49 -0400

    Before Express

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config"); // see ./config/config.js
 
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose
+    .connect(mongoURL, { // then following exists to prevent warnings. Doesn't really matter if it is there or not.
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log("succesfully connected to DB"))
+    .catch((e) => console.log(e));
+
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi There!!!!</h2>");
